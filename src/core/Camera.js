38d828fc226f16946a27fcf540b6c9b5ca1233d4
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-03 17:57:06 +0200

    feat: add object for sizes and add function to recalculate sizes

--- src/core/Camera.js
+++ src/core/Camera.js
@@ -1,11 +1,37 @@
 import * as THREE from "three";
+// Initialize Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  get aspect() {
+    return this.width / this.height;
+  },
+};
+
 // Initialize Camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  sizes.width / sizes.height,
   0.1,
   2000
 );
+
+// Set Camera Position
 camera.position.z = 5;
+
+// Calculates New Width and Height
+const updateSizes = () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+};
+const recalculateViewport = () => {
+  updateSizes();
+};
+
+// Updates Projection Matrix And Recalculates Viewport
+const updateCamera = () => {
+  recalculateViewport();
+  camera.updateProjectionMatrix();
+};
 
 export { camera };
