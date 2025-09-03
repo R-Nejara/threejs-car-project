3bfe392d8bbe29f360e954cb421bcb4c2e4184c4
Rayan Nejara <r.nejara@tu-braunschweig.de>
2025-09-03 21:17:54 +0200

    feat: add updateRenderer to renderloop

--- src/core/AnimationLoop.js
+++ src/core/AnimationLoop.js
@@ -1,14 +1,13 @@
-import * as THREE from "three";
-import { renderer } from "./Renderer.js";
-import { scene } from "./Scene.js";
-import { camera, updateCamera } from "./Camera.js";
+import { updateCamera } from "./Camera.js";
 import { updateControls } from "../controls/OrbitControls.js";
+import { updateRenderer } from "./Renderer.js"
 
 // Render Loop
 const runScene = () => {
   window.requestAnimationFrame(runScene);
-  renderer.render(scene, camera);
+  updateCamera();
   updateControls();
+  updateRenderer();
 };
 
 export { runScene };
