c2e6a2dcfd121922f48d19b078f703259fa60b86
Rayan Nejara <r.nejara@tu-braunschweig.de>
2025-09-03 21:17:17 +0200

    feat: add updateRenderer function

--- src/core/Renderer.js
+++ src/core/Renderer.js
@@ -1,12 +1,23 @@
 import * as THREE from "three";
 import { canvas } from "../Selectors.js";
+import { sizes, updateSizes } from "../utils/helpers.js";
+import { camera } from "./Camera.js";
+import { scene } from "./Scene.js";
+
 // Initialize Renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 
 // Renderer Size
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(sizes.width, sizes.height);
 
-// Set Pixelratio
+// Set Pixelratio   
 renderer.setPixelRatio = Math.min(2, devicePixelRatio);
 
-export { renderer };
+// Renderer Size Updater
+const updateRenderer = () => {
+  updateSizes();
+  renderer.render(scene, camera)
+  renderer.setSize(sizes.width, sizes.height)
+}
+
+export { renderer, updateRenderer };
