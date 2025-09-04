b44ffe2715cf661dd07dedc81b8eae20d9a8eb92
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 12:18:27 +0200

    feat: activate shadow casting and receiving for imported 3d models

--- src/core/Showcase.js
+++ src/core/Showcase.js
@@ -2,5 +2,10 @@ import * as THREE from "three";
 import { getObjectGroup } from "../loaders/glbLoader";
 
 const modelGroup = await getObjectGroup("/models/CarProject1.glb");
+modelGroup.traverse((child) => {
+  child.castShadow = true;
+  child.receiveShadow = true;
+});
+console.log(modelGroup);
 
 export { modelGroup };
