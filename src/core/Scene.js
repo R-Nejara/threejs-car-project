37fd6a8d545e478347dbb2c7000f8a0b34b61383
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 03:20:48 +0200

    feat: add light and model to scene

--- src/core/Scene.js
+++ src/core/Scene.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
 import { camera } from "./Camera.js";
-import { cubeMesh } from "./Car.js";
+import { modelGroup } from "./Showcase.js";
+import { lights } from "./Light.js";
 // Initialize Scene
 const scene = new THREE.Scene();
-
 // Addings To Scene
 scene.add(camera);
-scene.add(cubeMesh);
-
+scene.add(modelGroup);
+scene.add(lights);
 export { scene };
