c62dbfabf894d59caaea8a0d84df54457c53a37e
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 13:02:30 +0200

    feat: add directionalLightPosition vector and add directionalLightPosition to directionalLight

--- src/core/Light.js
+++ src/core/Light.js
@@ -1,6 +1,11 @@
 import * as THREE from "three";
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.castShadow = true;
+const directionalLightPosition = new THREE.Vector3(100, 10, 0);
+directionalLight.position.copy(directionalLightPosition);
+directionalLight.add(new THREE.AxesHelper(10));
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 
 const lights = new THREE.Group();
