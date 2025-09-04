8a4a9575063d94c8822458def78789a1d3ad6b18
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 12:46:23 +0200

    feat: call rendererShadowSetup in Renderer.js

--- src/core/Renderer.js
+++ src/core/Renderer.js
@@ -3,6 +3,7 @@ import { canvas } from "../Selectors.js";
 import { sizes, updateSizes } from "../utils/helpers.js";
 import { camera } from "./Camera.js";
 import { scene } from "./Scene.js";
+import { rendererShadowSetup } from "./Shadow.js";
 
 // Initialize Renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
@@ -19,5 +20,7 @@ const updateRenderer = () => {
   renderer.render(scene, camera);
   renderer.setSize(sizes.width, sizes.height);
 };
+rendererShadowSetup(renderer);
+updateRenderer();
 
 export { renderer, updateRenderer };
