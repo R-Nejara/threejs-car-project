f581013e342976ca6320c189347e9dd3db5a6561
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 03:22:45 +0200

    refactor: change Car.js to Showcase.js

--- src/core/Car.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from "three";
-// Test Object
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-export { cubeMesh };
