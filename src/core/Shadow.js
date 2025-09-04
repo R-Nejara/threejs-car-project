3f35236106bd298dee45ac5bd54e459af7115725
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-05 01:03:47 +0200

    feat: add directionalLightShadowSetup

--- src/core/Shadow.js
+++ src/core/Shadow.js
@@ -1,9 +1,11 @@
 import * as THREE from "three";
-import { renderer } from "./Renderer";
-import { PCFSoftShadowMap } from "three/src/constants.js";
 
 const rendererShadowSetup = (renderer) => {
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 };
-export { rendererShadowSetup };
+
+const directionalLightShadowSetup = (directionalLight) => {
+  directionalLight.shadow.normalBias = 0.2;
+};
+export { rendererShadowSetup, directionalLightShadowSetup };
