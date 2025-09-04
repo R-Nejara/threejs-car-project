40dc12af9422ab20df2ca37665d9fa4f7005ac20
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-05 01:00:47 +0200

    refactor: sort code for readability

--- src/core/Light.js
+++ src/core/Light.js
@@ -1,14 +1,14 @@
 import * as THREE from "three";
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-directionalLight.castShadow = true;
 const directionalLightPosition = new THREE.Vector3(10, 10, 0);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const lights = new THREE.Group();
+
 directionalLight.position.copy(directionalLightPosition);
+directionalLight.castShadow = true;
 directionalLight.add(new THREE.AxesHelper(10));
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-
-const lights = new THREE.Group();
 lights.add(directionalLight, ambientLight);
 
 export { lights, directionalLight };
