860e177d6481f09bf8c81e306796ea388e2d8701
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 12:58:30 +0200

    fix: correct light intensity for directionalLight

--- src/core/Light.js
+++ src/core/Light.js
@@ -1,15 +1,9 @@
 import * as THREE from "three";
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
-directionalLight.castShadow = true;
-// directionalLight.position.y = 10;
-// directionalLight.position.x = 100;
-directionalLight.add(new THREE.AxesHelper(10));
-
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 
 const lights = new THREE.Group();
-
 lights.add(directionalLight, ambientLight);
 
 export { lights, directionalLight };
