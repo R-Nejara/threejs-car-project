35583434256cfef1ce364eb4e0438fe425231525
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-04 03:18:27 +0200

    feat: add light

---
+++ src/core/Light.js
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+
+const lights = new THREE.Group();
+lights.add(directionalLight, ambientLight);
+
+export { lights };
